dd13fff5b05f45f3c8046e61e5a8701ab3285907
Majix <mimapien@gmail.com>
2018-07-26 18:31:57 +0430

    first commit for child theme of wikiwi

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+defined( 'ABSPATH' ) OR die( 'This script cannot be accessed directly.' );
+
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+function enqueue_parent_styles() {
+   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
