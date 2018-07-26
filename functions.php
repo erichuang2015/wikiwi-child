fc1e8601445bbe6447285c60f5d3eb44cede1f27
Majix <mimapien@gmail.com>
2018-07-26 18:40:24 +0430

    added theme version

--- functions.php
+++ functions.php
@@ -3,5 +3,6 @@ defined( 'ABSPATH' ) OR die( 'This script cannot be accessed directly.' );
 
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
-   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+    $theme = wp_get_theme();
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css', false, $theme->get( 'Version' ) );
 }
